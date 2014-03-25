36d0299074db67f6a7619670d7c2eb2b6d926001
Sam Sandberg <sam.sandberg@gmail.com>
2014-03-25 11:32:36 -0400

    Making brain live on heroku

--- js/application.js
+++ js/application.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
   console.log("READY!");
   window.requestAnimationFrame(function () {
-    brain = new AutonomousBrain("http://localhost:5000", 500, 3000);
+    brain = new AutonomousBrain("http://brain-2048.herokuapp.com", 500, 3000);
     new GameManager(4, brain, KeyboardInputManager, HTMLActuator, LocalStorageManager);
   });
 });
